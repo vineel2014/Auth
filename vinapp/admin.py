630c2b8452ccd577ed9b5b8f90ca63004a1f4b86
vineel2014 <vineel2006@gmail.com>
2017-05-21 22:39:09 +0530

    first commit

---
+++ vinapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from vinapp.models import Category,Page,UserProfile
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Page)
+admin.site.register(UserProfile)
+
---
+++ vinapp/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from vinapp import views
+
+urlpatterns=[
+            url(r"^$",views.index,name='index'),
+            url(r"^$",views.home,name='home'),
+            url(r"^register/$",views.register,name='register'),
+            url(r"^login/$",views.login,name='login'),
+            url(r"^logout/$",views.logout,name='logout'),
+            url(r'^restricted/', views.restricted, name='restricted'),
+]
